b529e79d5aa6ec4bafba417f4e5ee47b8cc611ff
Callum Askew <callumaskew@gmail.com>
2026-06-05 22:31:37 +0100

    Seperated mat2.cpp into mat2.h and mat2.cpp for consistency

---
+++ include/mat2.h
@@ -0,0 +1,31 @@
+class mat2{
+private:
+	const int rows = 2;
+	const int cols = 2;
+	float m[4];
+public:
+	// Default constructor
+	mat2();
+	
+	// Constructor with 4 values
+	mat2(float x0, float y0, float x1, float y1);
+	
+
+	// Returns the value at the specified position
+	float getValue(int row, int col) const;
+
+	// Changes the value at the specified position
+	void setValue(int row, int col, float value);
+
+
+	
+	// Matrix multiplication
+	mat2 multiply(const mat2& other) const;
+
+	// Matrix multiplication operator
+	mat2 operator*(const mat2& other) const;
+
+	
+	// Type casting
+	operator const float* () const;
+};
